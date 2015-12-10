db13178070b9fda10368dff154c04f5d6e98b6f4
mehdi <mehdi.rochdi@gmail.com>
2015-12-10 16:54:11 +0000

    update publish config file

--- src/PaylineServiceProvider.php
+++ src/PaylineServiceProvider.php
@@ -20,7 +20,7 @@ class PaylineServiceProvider extends ServiceProvider
 
         //define payline-sdk which are going to be published.
         $this->publishes([
-            __DIR__.'/config/payline.php' => base_path('config/laravel-payline-sdk.php')
+            __DIR__.'/config/laravel-payline-sdk.php' => base_path('config/laravel-payline-sdk.php')
         ]);
     }
 
